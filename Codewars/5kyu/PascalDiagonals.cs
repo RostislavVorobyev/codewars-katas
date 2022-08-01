c07dd0baef2c12ae895b3ad2c5649f1c8e5a226a
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-02 02:54:20 +0300

    5 kyu: Pascal's diagonals.

---
+++ Codewars/5kyu/PascalDiagonals.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace Codewars
+{
+    public class PascalDiagonals
+    {
+        public static BigInteger[] GenerateDiagonal(int n, int l)
+        {
+            if (l == 0)
+            {
+                return System.Array.Empty<BigInteger>();
+            }
+
+            var result = new BigInteger[l];
+            result[0] = 1;
+
+            for (int i = 1; i < l; i++)
+            {
+                var current = result[i - 1];
+                var next = GetNext(current, i, n);
+                result[i] = next;
+            }
+
+            return result;
+        }
+
+        private static BigInteger GetNext(BigInteger current, int i, int position) => current * position / i + current;
+    }
+}
