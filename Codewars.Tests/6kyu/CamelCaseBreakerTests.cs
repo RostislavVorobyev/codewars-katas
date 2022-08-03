ad6a72519a0b97643a0c781aae0c832cc2b1327c
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-04 01:36:49 +0300

    6 kyu: Break camelCase. Added test project for katas.

---
+++ Codewars.Tests/6kyu/CamelCaseBreakerTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class CamelCaseBreakerTests
+    {
+        private CamelCaseBreaker _camelCaseBreaker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _camelCaseBreaker = new CamelCaseBreaker();
+        }
+
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("camelCasing").Returns("camel Casing");
+                yield return new TestCaseData("camelCasingTest").Returns("camel Casing Test");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public string Test(string str) => _camelCaseBreaker.BreakCamelCase(str);
+    }
+}
---
+++ Codewars/6kyu/CamelCaseBreaker.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Break camelCase: https://www.codewars.com/kata/5208f99aee097e6552000148
+ * 
+ * This task can be solved much easier with [A-Z] Regex.
+ */
+using System.Linq;
+
+namespace Codewars
+{
+    public class CamelCaseBreaker
+    {
+        public string BreakCamelCase(string str)
+        {
+            return string.Join("", str.Select(x => char.IsUpper(x) ? $" {x}" : x.ToString()).ToArray());
+        }
+    }
+}
