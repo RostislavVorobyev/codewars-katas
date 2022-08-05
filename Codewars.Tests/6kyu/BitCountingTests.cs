9745975ea650b251854d3adb358d63deba00f648
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-05 11:21:07 +0300

    6 kyu : Bit Counting.

---
+++ Codewars.Tests/6kyu/BitCountingTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class BitCountingTests
+    {
+
+        private BitCounting _bitCounting;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bitCounting = new BitCounting();
+        }
+
+        [Test]
+        public void CountBits()
+        {
+            Assert.AreEqual(0, _bitCounting.CountBits(0));
+            Assert.AreEqual(1, _bitCounting.CountBits(4));
+            Assert.AreEqual(3, _bitCounting.CountBits(7));
+            Assert.AreEqual(2, _bitCounting.CountBits(9));
+            Assert.AreEqual(2, _bitCounting.CountBits(10));
+        }
+
+    }
+}
---
+++ Codewars/6kyu/BitCounting.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Bit Counting: https://www.codewars.com/kata/526571aae218b8ee490006f4
+ */
+using System;
+using System.Linq;
+
+namespace Codewars
+{
+    public class BitCounting
+    {
+        public int CountBits(int n)
+        {
+            return Convert.ToString(n, 2).Sum(x => x - '0');
+        }
+    }
+}
