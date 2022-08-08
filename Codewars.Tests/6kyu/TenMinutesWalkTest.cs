a899b82b1648f277f00ba2eed272c966185a76b9
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-08 22:32:18 +0300

    6 kuy: Take a Ten Minutes Walk.

---
+++ Codewars.Tests/6kyu/TenMinutesWalkTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Codewars
+{ 
+    [TestFixture]
+    public class SolutionTest
+    {
+
+    private TenMinutesWalk underTest = new TenMinutesWalk();
+
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(true, underTest.IsValidWalk(new string[] { "n", "s", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return true");
+            Assert.AreEqual(false, underTest.IsValidWalk(new string[] { "w", "e", "w", "e", "w", "e", "w", "e", "w", "e", "w", "e" }), "should return false");
+            Assert.AreEqual(false, underTest.IsValidWalk(new string[] { "w" }), "should return false");
+            Assert.AreEqual(false, underTest.IsValidWalk(new string[] { "n", "n", "n", "s", "n", "s", "n", "s", "n", "s" }), "should return false");
+        }
+    }
+}
---
+++ Codewars/6kyu/TenMinutesWalk.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Take a Ten Minutes Walk: https://www.codewars.com/kata/54da539698b8a2ad76000228/
+ */
+using System.Linq;
+
+namespace Codewars
+{
+    public class TenMinutesWalk
+    {
+        public bool IsValidWalk(string[] walk)
+        {
+            return walk.Length == 10 
+                && walk.Count(x => x == "n") == walk.Count(x => x == "s") 
+                && walk.Count(x => x == "e") == walk.Count(x => x == "w");
+        }
+    }
+}
