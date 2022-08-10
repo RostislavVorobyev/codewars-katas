d4a551aa325360769189c37165f3523799d023ae
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-10 22:27:25 +0300

    6 kyu : Does my number look big in this?

---
+++ Codewars.Tests/6kyu/NarcissticNumberTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class NarcissticNumberTests
+    {
+        private NarcissticNumber underTest = new NarcissticNumber();
+
+        [SetUp]
+        public void SetUp()
+        {
+            underTest = new NarcissticNumber();
+        }
+
+        private static IEnumerable<TestCaseData> TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(1)
+                                .Returns(true)
+                                .SetDescription("1 is narcissitic");
+                yield return new TestCaseData(371)
+                                .Returns(true)
+                                .SetDescription("371 is narcissitic");
+            }
+        }
+
+        [Test, TestCaseSource("TestCases")]
+        public bool Test(int n) => underTest.Narcissistic(n);
+
+    }
+}
---
+++ Codewars/6kyu/NarcissticNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars
+{
+    public class NarcissticNumber
+    {
+        public bool Narcissistic(int value)
+        {
+            var digits = value.ToString();
+
+            return digits.Sum(x => Math.Pow(x - '0', digits.Length)) == value;
+        }
+    }
+}
