e6d24b502a2d9e012329151b83efc05a84b20eef
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-14 03:17:09 +0300

    not enough

---
+++ Codewars.Tests/5kyu/PowerSumOfDigitsTests.cs
@@ -0,0 +1,20 @@
+﻿using Codewars;
+using NUnit.Framework;
+
+namespace CodewarsTests1
+{
+    [TestFixture]
+    public class PowerSumOfDigitsTests
+    {
+        private PowerSumOfDigits underTest = new PowerSumOfDigits();
+
+        [Test]
+        public void PowerSumDigTermShouldReturnCorrectAnswerForValidParameters()
+        {
+            //Assert.AreEqual(81, underTest.PowerSumDigTerm(1));
+            //Assert.AreEqual(512, underTest.PowerSumDigTerm(2));
+            Assert.AreEqual(2401, underTest.PowerSumDigTerm(3));
+            //Assert.AreEqual(4913, underTest.PowerSumDigTerm(4));
+        }
+    }
+}
---
+++ Codewars/5kyu/PowerSumOfDigits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars
+{
+    public class PowerSumOfDigits
+    {
+        public long PowerSumDigTerm(int n)
+        {
+            var properNumbers = new List<long>() { 81, 512, };
+
+            //i -разряд
+            for (int place = 2; properNumbers.Count < n; place++)
+            {
+                for (int sumOfDigits = 2; sumOfDigits < place * 9; sumOfDigits++)
+                {
+                    var number = sumOfDigits;
+
+
+                    while (number < Math.Pow(10, place) && properNumbers.Count != n)
+                    {
+                        if (number > properNumbers.Last() && SumDigits(number) == sumOfDigits)
+                        {
+                            properNumbers.Add(number);
+                        }
+                        number *= sumOfDigits;
+                    }
+                }
+            }
+
+            return properNumbers[n - 1];
+        }
+
+        public static int SumDigits(int n) => n.ToString().Select(x => x - '0').Sum();
+    }
+}
