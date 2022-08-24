d81603e3f32847d9b1a115ee636b6d16d52d31f4
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-25 01:18:28 +0300

    6 kuy : Array.diff

---
+++ Codewars.Tests/6kyu/ArrayDiffTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Codewars
+{
+    public class ArrayDiffTests
+    {
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void SampleTest()
+            {
+                Assert.AreEqual(new int[] { 2 }, ArrayDiff.Find(new int[] { 1, 2 }, new int[] { 1 }));
+                Assert.AreEqual(new int[] { 2, 2 }, ArrayDiff.Find(new int[] { 1, 2, 2 }, new int[] { 1 }));
+                Assert.AreEqual(new int[] { 1 }, ArrayDiff.Find(new int[] { 1, 2, 2 }, new int[] { 2 }));
+                Assert.AreEqual(new int[] { 1, 2, 2 }, ArrayDiff.Find(new int[] { 1, 2, 2 }, new int[] { }));
+                Assert.AreEqual(new int[] { }, ArrayDiff.Find(new int[] { }, new int[] { 1, 2 }));
+                Assert.AreEqual(new int[] { 3 }, ArrayDiff.Find(new int[] { 1, 2, 3 }, new int[] { 1, 2 }));
+            }
+        }
+    }
+}
---
+++ Codewars/6kyu/ArrayDiff.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Array.diff : https://www.codewars.com/kata/523f5d21c841566fde000009/solutions/csharp
+ */
+
+using System.Linq;
+
+namespace Codewars
+{
+    public static class ArrayDiff
+    {
+        public static int[] Find(int[] a, int[] b)
+        {
+            return a.Where(x => !b.Contains(x)).ToArray();
+        }
+    }
+}
