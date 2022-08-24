8b83cabebc7a2d52c46ae53c15e8ea06ba1e4a73
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-25 01:10:10 +0300

    7 kuy — Binary Addition.

---
+++ Codewars.Tests/7kyu/BinaryAddiction.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class AddBinaryTest
+    {
+        [Test]
+        public void TestExample()
+        {
+            Assert.AreEqual("11", BinaryAddition.AddBinary(1, 2));
+        }
+    }
+}
---
+++ Codewars/7kyu/BinaryAddition.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codewars
+{
+    public static class BinaryAddition
+    {
+        public static string AddBinary(int a, int b)
+        {
+            return Convert.ToString(a + b, 2);
+        }
+    }
+}
