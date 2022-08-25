5c6bf1bc3a76d41624e077405f9681100a40df2e
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-26 01:25:02 +0300

    5 kyu: Scramblies.

---
+++ Codewars.Tests/5kuy/ScrambliesTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class ScrambliesTests
+    {
+        [Test]
+        public static void ScrabliesReturnCorrectRestultWhenNonNullStringsPassed()
+        {
+            Assert.AreEqual(true, Scramblies.Scramble("rkqodlw", "world"));
+            Assert.AreEqual(true, Scramblies.Scramble("cedewaraaossoqqyt", "codewars"));
+            Assert.AreEqual(true, Scramblies.Scramble("katas", "steak"));
+            Assert.AreEqual(true, Scramblies.Scramble("scriptjavx", "javascript"));
+            Assert.AreEqual(true, Scramblies.Scramble("scriptingjava", "javascript"));
+            Assert.AreEqual(true, Scramblies.Scramble("scriptsjava", "javascripts"));
+            Assert.AreEqual(true, Scramblies.Scramble("javscripts", "javascript"));
+            Assert.AreEqual(true, Scramblies.Scramble("aabbcamaomsccdd", "commas"));
+        }
+    }
+}
---
+++ Codewars/5kyu/Scramblies.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public static class Scramblies
+    {
+        public static bool Scramble(string str1, string str2)
+        {
+            var str1LettersGroups = str2.GroupBy(x => x);
+            var str2LettersGroups = str1.GroupBy(x => x); 
+
+            foreach(var letterGroup in str1LettersGroups)
+            {
+                var sameLetterGroupInStr2 = str2LettersGroups.Where(x => x.Key == letterGroup.Key);
+
+                if (!sameLetterGroupInStr2.Any() || letterGroup.Count() > sameLetterGroupInStr2.First().Count())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
