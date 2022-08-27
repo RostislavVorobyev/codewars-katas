897545dd7cb54053e8b9a04b0524565e6ade0734
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-08-27 14:30:23 +0300

    6 kyu: IP validation

---
+++ Codewars.Tests/6kyu/IPValidationTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class IPValidationTests
+    {
+        [Test]
+        public void TestCases()
+        {
+            Assert.AreEqual(true, IPValidation.IsValid("0.0.0.0"));
+            Assert.AreEqual(true, IPValidation.IsValid("12.255.56.1"));
+            Assert.AreEqual(true, IPValidation.IsValid("137.255.156.100"));
+        }
+
+        [Test]
+        public void IsValidShouldReturnFalseOnIncorrectImput()
+        {
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.256.78"));
+            Assert.AreEqual(false, IPValidation.IsValid("1234.34.56"));
+            Assert.AreEqual(false, IPValidation.IsValid("pr12.34.56.78"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56.78sf"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56 .1"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56.-1"));
+            Assert.AreEqual(false, IPValidation.IsValid("123.045.067.089"));
+            Assert.AreEqual(false, IPValidation.IsValid(""));
+            Assert.AreEqual(false, IPValidation.IsValid("abc.def.ghi.jkl"));
+            Assert.AreEqual(false, IPValidation.IsValid("123.456.789.0"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56.00"));
+            Assert.AreEqual(false, IPValidation.IsValid("12.34.56.7.8"));
+        }
+    }
+}
---
+++ Codewars/6kyu/IPValidation.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    public static class IPValidation
+    {
+        public static bool IsValid(string ipAddres)
+        {
+            return Regex.Match(ipAddres, @"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$").Success;
+        }
+    }
+}
