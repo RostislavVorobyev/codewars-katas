1a2b6f02eeb1e60002202226fc41d3e3bd2f5fc8
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-11-13 23:19:43 +0200

    6 kyu: Your order, please.

---
+++ Codewars.Tests/6kyu/YourOrderPleaseTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class YourOrderPleaseTests
+    {
+        [Test, Description("Sample Tests")]
+        public void SampleTest()
+        {
+            Assert.AreEqual("Thi1s is2 3a T4est", YourOrderPlease.Order("is2 Thi1s T4est 3a"));
+            Assert.AreEqual("Fo1r the2 g3ood 4of th5e pe6ople", YourOrderPlease.Order("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.AreEqual("", YourOrderPlease.Order(""));
+        }
+    }
+}
---
+++ Codewars/6kyu/YourOrderPlease.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+/*
+ * Your order, please : https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/csharp
+ */
+namespace Codewars
+{
+    public class YourOrderPlease
+    {
+        public static string Order(string words)
+        {
+            if (words == string.Empty) 
+                return string.Empty;
+
+            var sortedWords = words.Split(" ").OrderBy(x => x.First(l => char.IsDigit(l)));
+            
+            return string.Join(" ", sortedWords);
+        }
+    }
+}
