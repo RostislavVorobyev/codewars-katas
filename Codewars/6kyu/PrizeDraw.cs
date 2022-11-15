f693152eb86d3135f6c05b3baaa2cf145a3865df
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-11-15 23:37:43 +0200

    6 kuy: Prize Draw

---
+++ Codewars/6kyu/PrizeDraw.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public class PrizeDraw
+    {
+        public static string NthRank(string st, int[] we, int n)
+        {
+            if (!st.Any())
+            {
+                return "No participants";
+            }
+
+            var names = st.Split(',');
+
+            if (n > names.Length)
+            {
+                return "Not enough participants";
+            }
+
+            return st.Split(',')
+                .Zip(we, (name, weight) => (name, (name.Sum(l => char.ToLower(l) - 96) + name.Length) * weight))
+                .OrderBy(nameValuePair => nameValuePair.Item2)
+                .ThenBy(nameValuePair => nameValuePair.name)
+                .Skip(n - 1)
+                .First().name;
+        }
+    }
+}
