d99fc4f34411c38781030847fe422655996e5cc1
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-11-16 21:54:51 +0200

    6 kuy: Prize Draw. Tests and solution fix.

---
+++ Codewars.Tests/6kyu/PrizeDrawTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests._6kyu
+{
+    [TestFixture]
+    public class PrizeDrawTests
+    {
+        public static string participants;
+        public static int[] weights;
+
+        [Test]
+        public static void NthRank_Should_Return_NoParticipants_On_Empty_Participants_List()
+        {
+            participants = string.Empty;
+            weights = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+            Assert.AreEqual("No participants", PrizeDraw.NthRank(participants, weights, 4));
+        }
+
+        [Test]
+        public static void NthRank_Should_Return_NoTEnoughParticipants_When_Weights_List_Less_Then_Participants_List()
+        {
+            participants = "Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin";
+            weights = new int[] { 4, 2, 1, 4, 3, 1, 2 };
+            Assert.AreEqual("Not enough participants", PrizeDraw.NthRank(participants, weights, 8));
+        }
+
+        [Test]
+        public static void NthRank_Should_Return_Participant_With_Rank_N()
+        {
+            participants = "Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden";
+            weights = new int[] { 1, 3, 5, 5, 3, 6 };
+            var n = 2;
+
+            Assert.AreEqual("Matthew", PrizeDraw.NthRank(participants, weights, n));
+        }
+    }
+}
--- Codewars/6kyu/PrizeDraw.cs
+++ Codewars/6kyu/PrizeDraw.cs
@@ -20,7 +20,7 @@ namespace Codewars
 
             return st.Split(',')
                 .Zip(we, (name, weight) => (name, (name.Sum(l => char.ToLower(l) - 96) + name.Length) * weight))
-                .OrderBy(nameValuePair => nameValuePair.Item2)
+                .OrderByDescending(nameValuePair => nameValuePair.Item2)
                 .ThenBy(nameValuePair => nameValuePair.name)
                 .Skip(n - 1)
                 .First().name;
