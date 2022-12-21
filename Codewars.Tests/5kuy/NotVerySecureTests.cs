fe4b8ccc48576af735d79f0c8aa536af956adbab
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-12-21 18:35:52 +0200

    5 kuy: not very secure

---
+++ Codewars.Tests/5kuy/NotVerySecureTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class Sample_Tests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Mazinkaiser").Returns(true);
+                yield return new TestCaseData("hello world_").Returns(false);
+                yield return new TestCaseData("PassW0rd").Returns(true);
+                yield return new TestCaseData("     ").Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => NotVerySecure.Alphanumeric(str);
+    }
+}
---
+++ Codewars/5kyu/NotVerySecure.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    public static class NotVerySecure
+    {
+        public static bool Alphanumeric(string str)
+        {
+            return Regex.Match(str, @"^[a-zA-Z0-9]+$").Success;
+        }
+    }
+}
+
