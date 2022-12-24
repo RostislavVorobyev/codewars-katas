d6bf72cfcce73abaa0f40cefe29fa1609bc5388e
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-12-24 02:00:26 +0200

    7 kuy: Survive the attack.

---
+++ Codewars/7kyu/SurviveTheAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public class SurviveTheAttack
+    {
+        public static bool HasSurvived(int[] attackers, int[] defenders)
+        {
+            var zip = Enumerable.Zip(attackers, defenders, (a, d) => d - a);
+
+            var defSurvivours = zip.Where(r => r > 0).Count();
+            var atackSurvivours = zip.Where(r => r < 0).Count();
+
+            var survivoursDif = defSurvivours - atackSurvivours + defenders.Length - attackers.Length;
+
+            if (survivoursDif == 0)
+            {
+                return defenders.Sum() >= attackers.Sum();
+            }
+
+            return survivoursDif > 0;
+        }
+    }
+}
