66a55af493c5b30e2c7f7d2376bd5ff106cbff1d
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-12-27 03:03:43 +0200

    6 kuy: Millipede of words

---
+++ Codewars/6kyu/MillipedeOfWords.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Codewars
+{
+    public static class MillipedeOfWords
+    {
+        public static bool Millipede(string[] arr)
+        {
+            Console.WriteLine(string.Join(" ", arr));
+            var unused = arr.GroupBy(x => x[0]).Select(x => string.Join(string.Empty, x)).ToList();
+
+            while (unused.Count > 1)
+            {
+                var curr = unused.First();
+                unused.Remove(curr);
+
+                var endJoin = unused.FirstOrDefault(x => x[0] == curr[^1]);
+                var strartJoin = unused.FirstOrDefault(x => x[^1] == curr[0]);
+
+                if (endJoin != null)
+                {
+                    unused.Remove(endJoin);
+                    unused.Add(curr + endJoin);
+                    continue;
+                }
+
+                if (strartJoin != null)
+                {
+                    unused.Remove(strartJoin);
+                    unused.Add(strartJoin + curr);
+                    continue;
+                }
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            var a = MillipedeOfWords.Millipede(new string[] { "excavate", "east", "strike", "tt" });
         }
     }
 }
+//effort cable evaluate
+//engine triangle expansion tablet cable   
+//trade transport tablet
+//transport engine elephant embrace strike traffic excavate
