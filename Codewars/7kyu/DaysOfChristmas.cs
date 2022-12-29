078cef2ba018f76269d781e878db47f1d8e46374
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-12-29 17:57:02 +0200

    7 kuy: DaysOfChristmas

---
+++ Codewars/7kyu/DaysOfChristmas.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * DaysOfChristmas: https://www.codewars.com/kata/57dd3a06eb0537b899000a64/train/csharp
+ */
+namespace Codewars
+{
+    public static class DaysOfChristmas
+    {
+        public static int Compare(string line1, string line2)
+        {
+            return Evaluate(line2[..2].TrimEnd()) - Evaluate(line2[..2].TrimEnd());
+        }
+
+        private static int Evaluate(string expr)
+        {
+            if (expr == "On")
+            {
+                return 1000;
+            }
+
+            if (expr == "a")
+            {
+                return 0;
+            }
+
+            return int.Parse(expr);
+        }
+    }
+}
