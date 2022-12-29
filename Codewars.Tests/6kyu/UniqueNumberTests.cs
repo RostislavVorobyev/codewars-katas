b32ed5974dd1d5f87ad38bcc96716e66131cfeb6
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2022-12-29 02:13:06 +0200

    6 kuy: Unique Number.

---
+++ Codewars.Tests/6kyu/UniqueNumberTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class UniqueNumberTests
+    {
+        [TestCase(new[] { 1, 2, 2, 2 }, ExpectedResult = 1)]
+        [TestCase(new[] { -2, 2, 2, 2 }, ExpectedResult = -2)]
+        [TestCase(new[] { 11, 11, 14, 11, 11 }, ExpectedResult = 14)]
+        public int BaseTest(IEnumerable<int> numbers)
+        {
+            return UniqueNumber.GetUnique(numbers);
+        }
+    }
+}
---
+++ Codewars/6kyu/UniqueNumber.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars
+{
+    public static class UniqueNumber
+    {
+        public static int GetUnique(IEnumerable<int> numbers)
+        {
+            return numbers.GroupBy(x => x).Where(x => x.Count() == 1).Single().Key;
+        }
+    }
+}
