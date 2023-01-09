9313caa8a6e204ac64ab526bde0ce2109418eb56
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-10 00:46:08 +0200

    6 kuy: Chaser's schedule.

---
+++ Codewars/6kyu/Chaser.cs
@@ -0,0 +1,20 @@
+﻿namespace Codewars
+{
+    public static class Chaser
+    {
+        public static int SpeedAndTime(int s, int t)
+        {
+            var distance = s * t;
+            var maxSprint = System.Math.Ceiling((double)t / 2);
+
+            for (var i = 0; i < maxSprint; i++)
+            {
+                if (s - 3 * i >= 0)
+                {
+                    distance += s - 3 * i;
+                }
+            }
+            return distance;
+        }
+    }
+}
--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -4,11 +4,6 @@
     {
         static void Main(string[] args)
         {
-            var a = MillipedeOfWords.Millipede(new string[] { "excavate", "east", "strike", "tt" });
         }
     }
 }
-//effort cable evaluate
-//engine triangle expansion tablet cable   
-//trade transport tablet
-//transport engine elephant embrace strike traffic excavate
