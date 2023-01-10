83abecca333a5805627a2752bf73c2acc41e21bf
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-10 23:55:03 +0200

    6 kuy: Sum of Digits / Digital Root.

---
+++ Codewars.Tests/6kyu/DigitalRootTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class DigitalRootTests
+    {
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(7, DigitalRoot.Calculate(16));
+            Assert.AreEqual(6, DigitalRoot.Calculate(456));
+        }
+    }
+}
---
+++ Codewars/6kyu/DigitalRoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public static class DigitalRoot
+    {
+        public static int Calculate(long n)
+        {
+            while (n / 10 > 0)
+            {
+                n = n.ToString().Sum(x => x - '0');
+            }
+
+            return (int)n;
+        }
+    }
+}
