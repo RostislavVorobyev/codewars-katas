1d5dfd2065e01ac2b3b41df3e4ae774de7556323
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-12 00:50:46 +0200

    7 kuy: Count Consonants.

---
+++ Codewars/7kyu/CountConsonants.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public static class CountConsonants
+    {
+        public static int ConsonantCount(string str)
+        {
+            var vowels = "aeiou";
+
+            bool isConsonant(char x) => char.IsLetter(x) && !vowels.Contains(char.ToLower(x));
+
+            return str.Count(isConsonant);
+        }
+    }
+}
