dd57b8030f9063dc206a736260e996484b937d01
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-13 00:41:24 +0200

    7 kuy: Adding values of arrays in a shifted way.

---
+++ Codewars/7kyu/ShiftedZip.cs
@@ -0,0 +1,25 @@
+﻿namespace Codewars
+{
+    public static class ShiftedZip
+    {
+        public static int[] AddingShifted(int[][] arrayOfArrays, int shift)
+        {
+            var arrLength = arrayOfArrays[0].Length;
+            int[] result = new int[arrLength + (arrayOfArrays.Length -1) * shift];
+
+            int currentShift = 0;
+
+            for (int i = 0; i < arrayOfArrays.Length; i++)
+            {
+                for (int j = 0; j < arrLength; j++)
+                {
+                    result[j + currentShift] += arrayOfArrays[i][j];
+                }
+
+                currentShift += shift;
+            }
+
+            return result;
+        }
+    }
+}
