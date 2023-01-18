762500881981891605e1cee668f03250d3f910c4
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-19 00:03:42 +0200

    5 kuy: Conway's Look and Say - Generalized 6 kuy: Who Likes It.

---
+++ Codewars.Tests/5kuy/ConwayLookAndSayTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Codewars.Tests
+{
+    using NUnit.Framework;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class ConwayLookAndSayTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(0ul).Returns(10ul).SetDescription("Input: 0ul\n      Expected: 10ul");
+                yield return new TestCaseData(2014ul).Returns(12101114ul).SetDescription("Input: 2014ul\n      Expected: 12101114ul");
+                yield return new TestCaseData(1122ul).Returns(2122ul).SetDescription("Input: 1122ul\n      Expected: 2122ul");
+                yield return new TestCaseData(22322ul).Returns(221322ul).SetDescription("Input: 22322ul\n      Expected: 221322ul");
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public ulong Test(ulong number) =>
+          ConwayLookAndSay.LookSay(number);
+    }
+}
---
+++ Codewars/5kyu/ConwayLookAndSay.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Codewars
+{
+    public static class ConwayLookAndSay
+    {
+        public static ulong LookSay(ulong number)
+        {
+            var numStr = number.ToString();
+            var prevDigit = numStr.ToString()[0];
+            var counter = 0;
+            var result = new StringBuilder();
+
+            foreach (var digit in numStr)
+            {
+                if (prevDigit == ' ')
+                    prevDigit = digit;
+
+                if (prevDigit == digit)
+                {
+                    counter++;
+                }
+                else
+                {
+                    result.Append($"{counter}{prevDigit}");
+                    prevDigit = digit;
+                    counter = 1;
+                }
+            }
+
+            if (result.Length == 0 || numStr[^1] != result[^1])
+            {
+                result.Append($"{counter}{prevDigit}");
+            }
+
+            return Convert.ToUInt64(result.ToString());
+        }
+    }
+}
