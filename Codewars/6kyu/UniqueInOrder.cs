59d98a0eb1f272f7d865ed1c88908da59b1b4f7c
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-19 23:47:24 +0200

    6 kuy: Unique In Order.

---
+++ Codewars/6kyu/UniqueInOrder.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars
+{
+    public static class UniqueInOrderKata
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            return iterable.Where((x, i) => i == 0 || !Equals(x, iterable.ElementAt(i - 1)));
+        }
+    }
+}
