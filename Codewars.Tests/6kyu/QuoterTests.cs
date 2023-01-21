3d86c5793ffb3da93d5214557d68a8ac9c60a488
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-21 23:37:45 +0200

    6 kuy : Greedy seller's quotes

---
+++ Codewars.Tests/6kyu/QuoterTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+
+namespace Codewars.Tests
+{
+    public class QuoterTests
+    {
+        [Test]
+        public void WhenQuotingFromEmptyStockReturnsInfoMessage()
+        {
+            var items = new List<Item>();
+            var quoter = GetQuoter(items);
+
+            Assert.AreEqual("Your order cannot be fulfilled, try lower quantity", quoter.GetQuote(11));
+        }
+
+        [Test]
+        public void WhenQuotingHighQuantityReturnsInfoMessage()
+        {
+            var items = new List<Item> { new Item { Quantity = 10, UnitPrice = 100m, UnitMargin = 1m } };
+            var quoter = GetQuoter(items);
+
+            Assert.AreEqual("Your order cannot be fulfilled, try lower quantity", quoter.GetQuote(11));
+        }
+
+        [Test]
+        public void WhenQuotingFromSingleItemReturnsCorrectPrice()
+        {
+            var items = new List<Item> { new Item { Quantity = 10, UnitPrice = 100m, UnitMargin = 1m } };
+            var quoter = GetQuoter(items);
+
+            Assert.AreEqual("Your best quote for 10 items is 1000.00", quoter.GetQuote(10));
+        }
+
+        [Test]
+        public void WhenQuotingFromTwoItemsReturnsHighestMargin()
+        {
+            var items = new List<Item>
+      {
+          new Item { Quantity = 10, UnitPrice = 100m, UnitMargin = 1m },
+          new Item { Quantity = 10, UnitPrice = 110m, UnitMargin = 1.1m }
+      };
+            var quoter = GetQuoter(items);
+
+            Assert.AreEqual("Your best quote for 10 items is 1100.00", quoter.GetQuote(10));
+        }
+
+        [Test]
+        public void WhenQuotingFromTwoItemsReturnsCombinationWithHighestMargin()
+        {
+            var items = new List<Item>
+      {
+          new Item { Quantity = 10, UnitPrice = 100m, UnitMargin = 1m },
+          new Item { Quantity = 10, UnitPrice = 110m, UnitMargin = 1.1m }
+      };
+            var quoter = GetQuoter(items);
+
+            Assert.AreEqual("Your best quote for 15 items is 1600.00", quoter.GetQuote(15));
+        }
+
+        private Quoter GetQuoter(List<Item> items)
+        {
+            return new Quoter(items);
+        }
+    }
+}
---
+++ Codewars/6kyu/Quoter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars
+{
+    public class Quoter
+    {
+        private List<Item> Items { get; set; }
+
+        public Quoter(List<Item> items)
+        {
+            Items = items;
+        }
+
+        public string GetQuote(int quantity)
+        {
+            var pricelist = new List<decimal>();
+            Items.OrderByDescending(c => c.UnitMargin).All(i => { pricelist.AddRange(Enumerable.Repeat(i.UnitPrice, i.Quantity)); return true; });
+            return pricelist.Count >= quantity ? string.Format("Your best quote for {0} items is {1}", quantity, pricelist.Take(quantity).Sum().ToString("#.00")) :
+            "Your order cannot be fulfilled, try lower quantity";
+
+            var orderedByMargin = Items.OrderByDescending(x => x.UnitMargin).ToArray();
+            decimal totalPrice = 0;
+            var itemsLeft = quantity;
+
+            for (var i = 0; i < Items.Count; i++)
+            {
+                var incr = Math.Min(orderedByMargin[i].Quantity, itemsLeft);
+
+                totalPrice += incr * orderedByMargin[i].UnitPrice;
+                itemsLeft -= incr;
+
+                if (itemsLeft == 0)
+                {
+                    return $"Your best quote for {quantity} items is {totalPrice:0.00}";
+                }
+            }
+
+            return "Your order cannot be fulfilled, try lower quantity";
+        }
+    }
+
+    public class Item
+    {
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal UnitMargin { get; set; }
+    }
+}
