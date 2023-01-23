f5decc6de2f3547977634b82ec75c21d0d57499a
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-24 01:06:45 +0200

    6 kuy : Invert binary tree.

---
+++ Codewars/6kyu/InvertBinaryTree.cs
@@ -0,0 +1,29 @@
+﻿namespace Codewars
+{
+    public static class InvertBinaryTree
+    {
+        public static TreeNode InvertTree(TreeNode root)
+        {
+
+            if (root == null)
+            {
+                return null;
+            }
+
+            var left = InvertTree(root.Left);
+            var right = InvertTree(root.Right);
+
+            root.Right = left;
+            root.Left = right;
+
+            return root;
+        }
+    }
+
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+    }
+}
