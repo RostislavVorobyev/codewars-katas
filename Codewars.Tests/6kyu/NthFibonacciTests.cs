96a63abbe65b2a0c96e679c8c12fba12b26cbe53
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-25 03:00:16 +0200

    6 kuy : N-th Fibonacci

---
+++ Codewars.Tests/6kyu/NthFibonacciTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class NthFibonacciTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData(1).Returns(0);
+                yield return new TestCaseData(2).Returns(1);
+                yield return new TestCaseData(3).Returns(1);
+                yield return new TestCaseData(4).Returns(2);
+                yield return new TestCaseData(5).Returns(3);
+                yield return new TestCaseData(6).Returns(5);
+                yield return new TestCaseData(7).Returns(8);
+                yield return new TestCaseData(8).Returns(13);
+                yield return new TestCaseData(9).Returns(21);
+                yield return new TestCaseData(10).Returns(24);
+                yield return new TestCaseData(11).Returns(55);
+                yield return new TestCaseData(12).Returns(89);
+                yield return new TestCaseData(13).Returns(144);
+                yield return new TestCaseData(14).Returns(233);
+                yield return new TestCaseData(15).Returns(377);
+                yield return new TestCaseData(16).Returns(610);
+                yield return new TestCaseData(17).Returns(987);
+                yield return new TestCaseData(18).Returns(1597);
+                yield return new TestCaseData(19).Returns(2584);
+                yield return new TestCaseData(20).Returns(4181);
+                yield return new TestCaseData(21).Returns(6765);
+                yield return new TestCaseData(22).Returns(10946);
+                yield return new TestCaseData(23).Returns(17711);
+                yield return new TestCaseData(24).Returns(28657);
+                yield return new TestCaseData(25).Returns(46368);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public int Test(int n)
+        {
+            return NthFibonacci.NthFib(n);
+        }
+    }
+}
---
+++ Codewars/6kyu/NthFibonacci.cs
@@ -0,0 +1,13 @@
+﻿namespace Codewars
+{
+    public static class NthFibonacci
+    {
+        public static int NthFib(int n)
+        {
+            if (n == 1) return 0;
+            if (n == 2) return 1;
+
+            return NthFib(n - 1) + NthFib(n - 2);
+        }
+    }
+}
