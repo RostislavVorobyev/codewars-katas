03de9149dbd077a09736debf6adfe5c6cef642e5
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-31 00:54:56 +0200

    6 kuy: Create Phone Number

---
+++ Codewars/6kyu/CreatePhoneNumber.cs
@@ -0,0 +1,11 @@
+﻿namespace Codewars
+{
+    public static class CreatePhoneNumber
+    {
+        public static string CreateNumber(int[] numbers)
+        {
+            var str = string.Join("", numbers);
+            return $"({str[0..2]} {str[2..5]}-{str[^4]})";
+        }
+    }
+}
