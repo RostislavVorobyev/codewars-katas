352df4f0b0b864815ab044b42fd207b88905b91e
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-01-31 23:37:12 +0200

    6 kuy: Consecutive Strings

---
+++ Codewars.Tests/6kyu/LongestConsecutivesTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Codewars.Tests
+{
+    [TestFixture]
+    public class LongestConsecutivesTests
+    {
+        private static void Testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public static void Test()
+        {
+            //Testing(ConsecutiveStrings.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { }, 3), "");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
+            Testing(ConsecutiveStrings.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
+        }
+    }
+}
---
+++ Codewars/6kyu/ConsecutiveStrings.cs
@@ -0,0 +1,23 @@
+﻿namespace Codewars
+{
+    public static class ConsecutiveStrings
+    {
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            var longesConsequence = string.Empty;
+
+            for (int i = 0; i <= strarr.Length - k; i++)
+            {
+                var l = i + k;
+                var currConsequence = string.Concat(strarr[i..l]);
+
+                if (longesConsequence.Length < currConsequence.Length)
+                {
+                    longesConsequence = currConsequence;
+                }
+            }
+
+            return longesConsequence;
+        }
+    }
+}
