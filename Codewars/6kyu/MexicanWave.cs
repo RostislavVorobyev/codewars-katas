0fdce330b5055234c0211cb04ddad937a37c8710
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-02-22 22:43:00 +0200

    6 kuy: Mexican Wave

---
+++ Codewars/6kyu/MexicanWave.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Codewars
+{
+    public static class MexicanWave
+    {
+        public static List<string> Wave(string str)
+        {
+            var result = new List<string>();
+
+            for (int x = 0; x < str.Length; x++)
+            {
+                var current = str[x];
+                if (char.IsLetter(current))
+                {
+                    result.Add(str[0..x] + char.ToUpper(current) + str[(x + 1)..]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
