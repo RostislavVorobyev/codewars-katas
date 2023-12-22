69a7bbd9a858c76ee249d3230a7a25d88586f48a
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2023-12-22 16:19:19 +0200

    6 kuy: Cuckoo Clock

---
+++ Codewars.Tests/6kyu/CuckooClockTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Codewars.Tests
+{
+    public class CuckooClockTests
+    {
+        [Test]
+        public void SimpleTests()
+        {
+            List<string> initialTimes = new List<string> { "07:22", "12:22", "01:30", "04:01", "03:38" };
+            List<int> chimes = new List<int> { 1, 2, 2, 10, 19 };
+            List<string> expectedTimes = new List<string> { "07:30", "12:45", "01:45", "05:30", "06:00" };
+
+            for (int i = 0; i < initialTimes.Count; i++)
+            {
+                Assert.AreEqual(expectedTimes[i], CuckooClock.CuckooClockCount(initialTimes[i], chimes[i]));
+            }
+        }
+
+        [Test]
+        public void HourTests() // Test case where the starting time is 00, i.e. on the hour
+        {
+            List<string> initialTimes = new List<string> { "10:00", "10:00", "10:00", "10:00", "10:00" };
+            List<int> chimes = new List<int> { 1, 10, 11, 13, 20 };
+            List<string> expectedTimes = new List<string> { "10:00", "10:00", "10:15", "10:45", "11:00" };
+
+            for (int i = 0; i < initialTimes.Count; i++)
+            {
+                Assert.AreEqual(expectedTimes[i], CuckooClock.CuckooClockCount(initialTimes[i], chimes[i]));
+            }
+        }
+
+        [Test]
+        public void TwelveTests() // Test going from twelve to one
+        {
+            List<string> initialTimes = new List<string> { "12:30", "12:30", "12:30", "12:30", "09:53" };
+            List<int> chimes = new List<int> { 1, 2, 3, 4, 50 };
+            List<string> expectedTimes = new List<string> { "12:30", "12:45", "01:00", "01:15", "02:30" };
+
+            for (int i = 0; i < initialTimes.Count; i++)
+            {
+                Assert.AreEqual(expectedTimes[i], CuckooClock.CuckooClockCount(initialTimes[i], chimes[i]));
+            }
+        }
+
+        [Test]
+        public void AroundTheClockTests() // Test going more than 12 hours ahead
+        {
+            List<string> initialTimes = new List<string> { "08:17", "08:17", "08:17", "08:17", "08:17" };
+            List<int> chimes = new List<int> { 113, 114, 115, 150, 200 };
+            List<string> expectedTimes = new List<string> { "08:00", "08:15", "08:30", "11:00", "05:45" };
+
+            for (int i = 0; i < initialTimes.Count; i++)
+            {
+                Assert.AreEqual(expectedTimes[i], CuckooClock.CuckooClockCount(initialTimes[i], chimes[i]));
+            }
+        }
+    }
+}
---
+++ Codewars/6kyu/CuckooClock.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * https://www.codewars.com/kata/656e4602ee72af0017e37e82
+ */
+
+using System;
+using System.Linq;
+
+namespace Codewars
+{
+    public static class CuckooClock
+    {
+
+        public static int[] cuckooSchedule = {
+            1,1,1,1
+            ,2,1,1,1
+            ,3,1,1,1
+            ,4,1,1,1
+            ,5,1,1,1
+            ,6,1,1,1
+            ,7,1,1,1
+            ,8,1,1,1
+            ,9,1,1,1
+            ,10,1,1,1
+            ,11,1,1,1
+            ,12,1,1,1
+        };
+
+        public static string CuckooClockCount(string inputTime, int chimes)
+        {
+            var finishTime = DateTime.Parse(inputTime);
+
+            var quater = (finishTime.Hour - 1) * 4 + finishTime.Minute / 15 + 1;
+
+            var chimesNeeded = chimes;
+
+            if (finishTime.Minute % 15 == 0)
+            {
+                chimesNeeded -= cuckooSchedule[quater - 1];
+                if (chimesNeeded < 0) return inputTime;
+            }
+
+            if (chimesNeeded > 113)
+            {
+                var perDay = cuckooSchedule.Sum();
+                chimesNeeded %= perDay;
+            }
+
+            for (var q = quater; chimesNeeded > 0; q++)
+            {
+                if (q == cuckooSchedule.Length)
+                    q = 0;
+
+                finishTime = finishTime.AddMinutes(15);
+
+                chimesNeeded -= cuckooSchedule[q];
+            }
+
+            finishTime = finishTime.AddMinutes(-(finishTime.Minute % 15));
+
+            return finishTime.ToString("hh:mm");
+        }
+    }
+}
