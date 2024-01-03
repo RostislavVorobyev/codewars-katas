6c1d2111f33f7621148e002a4e8d1ac5e1a63a7d
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2024-01-03 18:32:59 +0200

    6 kuy: English beggars

---
+++ Codewars.Tests/6kyu/EnglishBeggarsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests
+{
+    public class EnglishBeggarsTests
+    {
+        [Test]
+        public void FixedTest()
+        {
+            // Basic tests
+            int[] test = { 1, 2, 3, 4, 5 };
+            int[] a1 = { 15 }, a2 = { 9, 6 }, a3 = { 5, 7, 3 }, a4 = { 1, 2, 3, 4, 5, 0 }, a5 = { };
+            Assert.AreEqual(a1, EnglishBeggars.Beggars(test, 1));
+            Assert.AreEqual(a2, EnglishBeggars.Beggars(test, 2));
+            Assert.AreEqual(a3, EnglishBeggars.Beggars(test, 3));
+            Assert.AreEqual(a4, EnglishBeggars.Beggars(test, 6));
+            Assert.AreEqual(a5, EnglishBeggars.Beggars(test, 0));
+        }
+    }
+}
---
+++ Codewars/6kyu/EnglishBeggars.cs
@@ -0,0 +1,19 @@
+﻿namespace Codewars
+{
+    public static class EnglishBeggars
+    {
+        public static int[] Beggars(int[] values, int n)
+        {
+            var result = new int[n];
+
+            if (n == 0) { return result; }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                result[i % n] += values[i];
+            }
+
+            return result;
+        }
+    }
+}
