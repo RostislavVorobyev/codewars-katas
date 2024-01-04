6035029b1879f7e72af341dde1674ecb99cce6d3
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2024-01-04 13:23:02 +0200

    6 kuy: SalesmanTravel

---
+++ Codewars/6kyu/SalesmanTravel.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+
+namespace Codewars
+
+{
+    public static class SalesmanTravel
+    {
+        public static string Travel(string r, string zipcode)
+        {
+            var addressGroups = r.Split(',')
+                .GroupBy(x => x[^8..])
+                .Where(x => x.Key == zipcode)
+                .FirstOrDefault();
+
+            return addressGroups != null ?
+                $"{zipcode}:{string.Join(',', addressGroups.Select(x => $"{x[(x.IndexOf(' ') + 1)..(x.IndexOf(zipcode) - 1)]}"))}" +
+                $"/{string.Join(',', addressGroups.Select(x => x[..x.IndexOf(' ')]))}"
+                : $"{zipcode}:/";
+        }
+
+    }
+}
