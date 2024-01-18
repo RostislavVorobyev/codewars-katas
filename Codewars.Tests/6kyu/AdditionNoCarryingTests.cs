85a266f38f43d0b1a66cc95b2711d0b2a593006a
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2024-01-18 23:06:12 +0200

    6 kuy: Addition without Carrying

---
+++ Codewars.Tests/6kyu/AdditionNoCarryingTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Tests._6kyu
+{
+    [TestFixture]
+    public class AdditionNoCarryingTests
+    {
+
+        [Test]
+        public void BasicTests()
+        {
+            var kata = new AdditionNoCarrying();
+
+            Assert.AreEqual(1180, kata.AdditionWithoutCarrying(456, 1734), "");
+
+            Assert.AreEqual(99999, kata.AdditionWithoutCarrying(99999, 0), "");
+
+            Assert.AreEqual(888, kata.AdditionWithoutCarrying(999, 999), "");
+
+            Assert.AreEqual(0, kata.AdditionWithoutCarrying(0, 0), "");
+        }
+    }
+}
---
+++ Codewars/6kyu/AdditionNoCarrying.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Codewars
+{
+    public class AdditionNoCarrying
+    {
+        public int AdditionWithoutCarrying(int a, int b)
+        {
+            var sum = 0;
+            var l = Math.Max(a, b).ToString().Length;
+
+            for (int i = 0; i < l; i++)
+            {
+                sum += (int)(((a % 10 + b % 10) % 10) * Math.Pow(10, i-1));
+                a /= 10;
+                b/= 10;
+            }
+
+            return sum;
+        }
+    }
+}
