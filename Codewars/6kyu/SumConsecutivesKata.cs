dd7aca3763c63775793f5b71f8b8eeda2f1e88d4
RostislavVorobyov <Rostislav.Vorobyev@mail.ru>
2024-01-22 22:45:18 +0200

    6 kuy: Sum Consecutives

---
+++ Codewars/6kyu/SumConsecutivesKata.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Codewars
+{
+    public class SumConsecutivesKata
+    {
+        public static List<int> SumConsecutives(List<int> s)
+        {
+            if (s.Count == 0)
+                return new List<int>();
+
+            var result = new List<int>() { s[0] };
+
+            for (int i = 1; i < s.Count; i++)
+            {
+                if (s[i] == s[i - 1])
+                    result[^1] = result[^1] + s[i];
+                else
+                    result.Add(s[i]);
+            }
+
+            return result;
+        }
+    }
+}
